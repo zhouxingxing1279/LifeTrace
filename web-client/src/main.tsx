c914f8ad155f1bf8662a55a174c311cf25648eeb
zhouxingxing1279 <76243394+zhouxingxing1279@users.noreply.github.com>
2026-08-06 16:50:02 +0800

    chore(epic13): initialize cloud scopes and responsive styles

--- web-client/src/main.tsx
+++ web-client/src/main.tsx
@@ -1,7 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "./bootstrap";
 import App from "./App";
 import "./styles.css";
+import "./epic13.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
