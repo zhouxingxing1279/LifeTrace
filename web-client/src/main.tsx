6bfb05b9cba36fad5b0b54be94d7e28f4e52b48f
zhouxingxing1279 <76243394+zhouxingxing1279@users.noreply.github.com>
2026-08-07 10:06:14 +0800

    feat(observability): install request instrumentation at browser startup

--- web-client/src/main.tsx
+++ web-client/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import ClientErrorBoundary from "../../src/components/ClientErrorBoundary";
 import { installGlobalErrorHandlers } from "../../src/services/clientObservability";
+import { installGlobalFetchInstrumentation } from "../../src/services/fetchInstrumentation";
 import "./bootstrap";
 import App from "./App";
 import "../../app/globals.css";
@@ -17,6 +18,7 @@ import "./styles.css";
 import "./cloud-pages.css";
 import "./browser.css";
 
+installGlobalFetchInstrumentation();
 installGlobalErrorHandlers();
 
 createRoot(document.getElementById("root")!).render(
